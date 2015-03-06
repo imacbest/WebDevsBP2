336619a02beb41f494c44906392b26bb6bc9f49a
imacbest <thomas.erin999@gmail.com>
2015-03-06 12:58:51 +0100

    excel

--- pages/producten.php
+++ pages/producten.php
@@ -25,6 +25,7 @@
 $db->openDBConnection();
 $tsql = "SELECT * FROM PRODUCT";
 $result = sqlsrv_query( $db->getConn(), $tsql) or die( print_r( sqlsrv_errors() )  );
+echo "Num rows" . sqlsrv_num_rows($result);
 while( $row = sqlsrv_fetch_array( $result, SQLSRV_FETCH_ASSOC)){
     echo "<div class=\"product\">";
     echo "  <a href='?product&pid=".$row['PRODUCTNUMMER']."' title='".$row['PRODUCTNAAM']."'>
