19673d316295fe93175ba0dfb3806bd4f65d3532
imacbest <thomas.erin999@gmail.com>
2015-03-09 15:42:26 +0100

    removed junk

--- pages/winkelwagen.php
+++ pages/winkelwagen.php
@@ -8,18 +8,3 @@
 ?>
 <h1>Winkelwagen</h1>
 
-<?php
-$string = "Hello World!";
-
-for($i = 0; $i < strlen($string); $i++){
-    echo $string[$i] . "<br />";
-}
-
-echo "<br />";
-for($i = strlen($string)-1; $i > 0; $i--){
-    echo $string[$i] . "<br />";
-}
-
-
-
-?>
