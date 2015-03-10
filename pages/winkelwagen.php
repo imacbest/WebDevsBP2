905abbc5857d685d6ff1a3c8a2cac1c525871913
imacbest <thomas.erin999@gmail.com>
2015-03-10 09:48:16 +0100

    cart stuff

--- pages/winkelwagen.php
+++ pages/winkelwagen.php
@@ -15,7 +15,11 @@ $_SESSION['winkelwagen'][0]['productID'] = 55;
 $_SESSION['winkelwagen'][0]['aantal'] = 5;
 $_SESSION['winkelwagen'][1]['productID'] = 2;
 $_SESSION['winkelwagen'][1]['aantal'] = 3;
-//print_r($_SESSION);
+addToCart(33, 5);
+addToCart(43, 5);
+print_r($_SESSION);
+echo "<br />";
+
 if(array_key_exists('winkelwagen', $_SESSION)){
     $items = count($_SESSION['winkelwagen']);
     if($items > 0){
@@ -30,5 +34,26 @@ if(array_key_exists('winkelwagen', $_SESSION)){
 }else{
     echo "Geen array gevonden "; // geen items in winkelwagen
 }
-?>
 
+echo checkIfProductInCart(55);
+
+function addToCart($productID, $amount){
+    if(checkIfProductInCart($productID) != false){
+        $_SESSION['winkelwagen'][checkIfProductInCart($productID)]['aantal']  += $amount;
+    }else{
+        $inserPoint = max(array_keys($_SESSION['winkelwagen'])) +1;
+        $_SESSION['winkelwagen'][$inserPoint]['aantal'] = $amount;
+        $_SESSION['winkelwagen'][$inserPoint]['productID'] = $productID;
+    }
+}
+
+function checkIfProductInCart($productID){
+    for($i = 0; $i < count($_SESSION['winkelwagen']); $i++){
+        if($_SESSION['winkelwagen'][$i]['productID'] == $productID){
+            return $i;
+        }
+    }
+    return false;
+}
+
+?>
