26a913654f4cc3e900957bebe27cf29e97d33ade
imacbest <thomas.erin999@gmail.com>
2015-03-10 10:07:19 +0100

    winkelwagen

--- pages/winkelwagen.php
+++ pages/winkelwagen.php
@@ -15,15 +15,18 @@ $_SESSION['winkelwagen'][0]['productID'] = 55;
 $_SESSION['winkelwagen'][0]['aantal'] = 5;
 $_SESSION['winkelwagen'][1]['productID'] = 2;
 $_SESSION['winkelwagen'][1]['aantal'] = 3;
-addToCart(33, 5);
-addToCart(43, 5);
-print_r($_SESSION);
+//addToCart(33, 5);
+//addToCart(43, 5);
+echo removeAllFromCart(33);
+
+
 echo "<br />";
 
 if(array_key_exists('winkelwagen', $_SESSION)){
     $items = count($_SESSION['winkelwagen']);
     if($items > 0){
         for($i = 0; $i < $items; $i++){
+            if($_SESSION['winkelwagen'][$i]['aantal'] != 0)
             echo "Product id: " . $_SESSION['winkelwagen'][$i]['productID'] . " Aantal: " . $_SESSION['winkelwagen'][$i]['aantal'];
             echo "<br />";
         }
@@ -35,15 +38,25 @@ if(array_key_exists('winkelwagen', $_SESSION)){
     echo "Geen array gevonden "; // geen items in winkelwagen
 }
 
-echo checkIfProductInCart(55);
+function removeAllFromCart($productID){
+        for($i = 0; $i < count($_SESSION['winkelwagen']); $i++){
+            print_r($_SESSION);
+            echo  "<br />" . $_SESSION['winkelwagen'][$i]['productID'];
+            if($_SESSION['winkelwagen'][$i]['productID'] == $productID){
+                $_SESSION['winkelwagen'][$i]['aantal'] = 0;
+                return "Product is verwijderd.";
+            }
+        }
 
+
+}
 function addToCart($productID, $amount){
     if(checkIfProductInCart($productID) != false){
         $_SESSION['winkelwagen'][checkIfProductInCart($productID)]['aantal']  += $amount;
     }else{
-        $inserPoint = max(array_keys($_SESSION['winkelwagen'])) +1;
-        $_SESSION['winkelwagen'][$inserPoint]['aantal'] = $amount;
-        $_SESSION['winkelwagen'][$inserPoint]['productID'] = $productID;
+        $insertPoint = max(array_keys($_SESSION['winkelwagen'])) +1;
+        $_SESSION['winkelwagen'][$insertPoint]['productID'] = $productID;
+        $_SESSION['winkelwagen'][$insertPoint]['aantal'] = $amount;
     }
 }
 
