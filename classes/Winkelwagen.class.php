312381688a1f153a229b3e1c3b7cc20dbe04319f
imacbest <thomas.erin999@gmail.com>
2015-03-10 11:42:24 +0100

    winkelwagen

--- classes/Winkelwagen.class.php
+++ classes/Winkelwagen.class.php
@@ -35,8 +35,12 @@ class Winkelwagen {
     }
     public function addToCart($productID, $amount){
         if(array_key_exists('winkelwagen', $_SESSION)) {
-            if ($this->checkIfProductInCart($productID) != false) {
-                $_SESSION['winkelwagen'][$this->checkIfProductInCart($productID)]['aantal'] += $amount;
+            if ($this->checkIfProductInCart($productID)) {
+                for ($i = 0; $i < count($_SESSION['winkelwagen']); $i++) {
+                    if ($_SESSION['winkelwagen'][$i]['productID'] == $productID) {
+                        $_SESSION['winkelwagen'][$i]['aantal'] += $amount;
+                    }
+                }
             } else {
                 $insertPoint = max(array_keys($_SESSION['winkelwagen'])) + 1;
                 $_SESSION['winkelwagen'][$insertPoint]['productID'] = $productID;
@@ -52,9 +56,22 @@ class Winkelwagen {
     public function checkIfProductInCart($productID){
         for ($i = 0; $i < count($_SESSION['winkelwagen']); $i++) {
             if ($_SESSION['winkelwagen'][$i]['productID'] == $productID) {
-                return $i;
+                return true;
             }
         }
         return false;
+    }
+
+    public function isCartEmpty(){
+        if(array_key_exists('winkelwagen', $_SESSION)) {
+            for ($i = 0; $i < count($_SESSION['winkelwagen']); $i++) {
+                if($_SESSION['winkelwagen'][$i]['aantal'] != 0){
+                    return false;
+                }
+            }
+            return true;
+        }else{
+            return true;
+        }
     }
 }
--- pages/winkelwagen.php
+++ pages/winkelwagen.php
@@ -6,36 +6,60 @@
  * Time: 10:47
  */
 
+if(isset($_GET['removeID'])){
+    if(!empty($_GET['removeID'])){
+        $cart->removeAllFromCart($_GET['removeID']);
+        echo "Product is verwijderd uit je winkelwagentje!";
+    }
+}
+
 require("classes/Product.class.php");
-$cart->addToCart(1, 5);
-$cart->addToCart(2, 5);
-$cart->addToCart(10, 5);
+
+
 ?>
 <h1>Winkelwagen</h1>
 
 
 <?php
-
-if(array_key_exists('winkelwagen', $_SESSION)) {
-    $items = count($_SESSION['winkelwagen']);
-    if ($items > 0) {
-        for ($i = 0; $i < $items; $i++) {
-            if ($_SESSION['winkelwagen'][$i]['aantal'] != 0) {
-                $product = new Product($_SESSION['winkelwagen'][$i]['productID'], $db);
-                echo "Product naam: " .$product->getProductNaam() . " Aantal: " . $_SESSION['winkelwagen'][$i]['aantal'];
-                echo "<br />";
+if($cart->isCartEmpty()){
+    echo "Het winkelwagentje is nog leeg, je kunt op de <a href='?producten' title='producten'>producten pagina</a> onze producten vinden."; // geen items in winkelwagen
+}else {
+?>
+    <table class="winkelwagen">
+    <tr>
+        <th>&nbsp;</th>
+        <th>Productnaam</th>
+        <th>Prijs</th>
+        <th>Aantal</th>
+        <th>Subtotaal</th>
+        <th>Verwijderen</th>
+    </tr>
+    <?php
+    if (array_key_exists('winkelwagen', $_SESSION)) {
+        $items = count($_SESSION['winkelwagen']);
+        if ($items > 0) {
+            for ($i = 0; $i < $items; $i++) {
+                if ($_SESSION['winkelwagen'][$i]['aantal'] != 0) {
+                    $product = new Product($_SESSION['winkelwagen'][$i]['productID'], $db);
+                    ?>
+                <tr>
+                    <td><a href="?product&amp;pid=<?= $_SESSION['winkelwagen'][$i]['productID']; ?>" title="<?= $product->getProductNaam(); ?>"><img src="media/producten/<?= $product->getImg(); ?>" class="imgPreview" alt="Jack" height="250" /></a> </td>
+                    <td><a href="?product&amp;pid=<?= $_SESSION['winkelwagen'][$i]['productID']; ?>" title="<?= $product->getProductNaam(); ?>"><?= $product->getProductNaam(); ?></a></td>
+                    <td>&euro; <?= $product->getPrijs(); ?></td>
+                    <td><?= $_SESSION['winkelwagen'][$i]['aantal']; ?> </td>
+                    <td>&euro; <?= $product->getPrijs() * $_SESSION['winkelwagen'][$i]['aantal']; ?> </td>
+                    <td><a href="?winkelwagen&amp;removeID=<?= $_SESSION['winkelwagen'][$i]['productID']; ?>" title="Verwijder <?= $product->getProductNaam(); ?> uit je winkelwagentje">Verwijder</a> </td>
+                </tr>
+                <?php
+                }
             }
-        }
+            echo "</table> ";
 
-        }else{
+        } else {
             echo "Geen items gevonden";
         }
     } else {
-        echo "Geen array gevonden "; // geen items in winkelwagen
+        echo "Het winkelwagentje is nog leeg, je kunt op de <a href='?producten' title='producten'>producten pagina</a> onze producten vinden."; // geen items in winkelwagen
+    }
 }
-
-
-echo "<br /><br />";
-print_r($_SESSION);
-
 ?>
