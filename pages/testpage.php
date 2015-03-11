d2f81b3335cc45a674f244b4cc5a27f42b79e503
imacbest <thomas.erin999@gmail.com>
2015-03-11 11:52:43 +0100

    edit insert script

--- pages/testpage.php
+++ pages/testpage.php
@@ -2,7 +2,16 @@
 
 for($i = 1; $i < 41; $i++){
     for($j = 1; $j < 4; $j++){
-        echo "INSERT [dbo].[PRODUCT_GERELATEERD_PRODUCT] ([PRODUCTNUMMER], [PRODUCTNUMMER_GERELATEERD_PRODUCT]) VALUES (".$i.", ".$i + $j.") <br />";
+        if($i != 40){
+            $related = $i + $j;
+        }else{
+            $related = 0 + $j;
+        }
+        echo "INSERT [dbo].[PRODUCT_GERELATEERD_PRODUCT] ([PRODUCTNUMMER], [PRODUCTNUMMER_GERELATEERD_PRODUCT]) VALUES (";
+        echo $i;
+        echo ", ";
+        echo $related;
+        echo ") <br />";
     }
 }
 
