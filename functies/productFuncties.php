f3d762659818386d684fd13b141c4e8d89daaadb
maxgroen <maxgroenendijk@outlook.com>
2015-03-12 14:33:57 +0100

    product edit

--- functies/productFuncties.php
+++ functies/productFuncties.php
@@ -3,7 +3,7 @@
 
 function productnaam_check($naam) {
     global $db;
-    $naam_query = "SELECT PRODUCTNAAM FROM PRODUCT WHERE PRODUCTNAAM='" . $naam . "';";
+    $naam_query = "SELECT PRODUCTNAAM FROM PRODUCT WHERE PRODUCTNAAM='" . $naam . "'";
     $naam_sql = sqlsrv_query($db->getConn(), $naam_query) or die (print_r(sqlsrv_errors()));
     if (!sqlsrv_has_rows($naam_sql)) {
         return false;
@@ -18,5 +18,13 @@ function prijs_check($prijs) {
     return false;
 }
 
+function createProduct($productnaam, $omschrijving, $categorie, $prijs, $voorraad,  $inhoud, $afbeelding){
+    global $db;
+    $sql="INSERT INTO PRODUCT ( PRODUCTNAAM, OMSCHRIJVING, CATEGORIE, PRIJS, VOORRAAD, INHOUD, afbeelding)
+    VALUES('".addslashes($productnaam)."', '".addslashes($omschrijving)."', '".addslashes($categorie)."','".addslashes($prijs)."', '".addslashes($voorraad)."',
+    '".addslashes($inhoud)."', '".addslashes($afbeelding)."')";
+    sqlsrv_query($db->getConn(), $sql, NULL) or die (print_r(sqlsrv_errors()));
+
+}
 
 ?>
--- pages/addProduct-ac.php
+++ pages/addProduct-ac.php
@@ -13,27 +13,25 @@ $msg = "";
 if(empty($_POST['Productnaam'])) {
     $msg .= "Productnaam is niet ingevuld!<br />";
     $countError++;
-} else if(productnaam_check($_POST['Productnaam'])) {
-    $msg .= "De productnaam bestaat al!<br />";
-    $countError++;
-}
+} //else if(!productnaam_check($_POST['Productnaam'])) {
+    //$msg .= "De productnaam bestaat al!<br />";
+    //$countError++;
+//}
 if(empty($_POST['Omschrijving'])) {
     $msg .= "Omschrijving is niet ingevuld!<br />";
     $countError++;
 }
-echo $_POST['Categorie'];
 if(empty($_POST['Categorie'])) {
     $msg .= "Catergorie is niet ingevuld!<br />";
     $countError++;
 }
-echo $_POST['Prijs'];
 if(empty($_POST['Prijs'])) {
     $msg .= "Prijs is niet ingevuld!<br />";
     $countError++;
-} else if(!prijs_check($_POST['Prijs'])) {
-    $msg .= "De prijs mag alleen uit nummer bestaan!<br />";
-    $countError++;
-}
+} //else if(!prijs_check($_POST['Prijs'])) {
+    //$msg .= "De prijs mag alleen uit nummer bestaan!<br />";
+    //$countError++;
+//}
 if(empty($_POST['Voorraad'])) {
     $msg .= "Voorraad is niet ingevuld!<br />";
     $countError++;
@@ -60,5 +58,10 @@ if($countError == 1) {
 }else if($countError != 0) {
     echo "<br ?>Er zijn in totaal " . $countError . " fouten gevonden!<br />";
     echo '<a href=?addProduct title="Terug naar de vorige pagina">Terug naar de vorige pagina</a>';
+}else if($countError == 0) {
+    echo "Het product is succesvol toegevoegd!";
+    echo '<a href=?adminProducts title="Terug naar de beheerpagina">Terug naar de beheerpagina</a>';
+    createProduct($_POST['Productnaam'], $_POST['Omschrijving'], $_POST['Categorie'], $_POST['Prijs'],
+        $_POST['Voorraad'], $_POST['Inhoud'], $_POST['Afbeelding']);
 }
 ?>
