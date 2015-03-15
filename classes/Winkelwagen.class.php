818412920636be9cfa9e2a45255fd865f09ed6fa
imacbest <thomas.erin999@gmail.com>
2015-03-15 16:30:15 +0100

    small improvement

--- classes/Winkelwagen.class.php
+++ classes/Winkelwagen.class.php
@@ -84,6 +84,8 @@ class Winkelwagen{
         } else if($inCart + $amount > $this->getVoorraad()) {
            $amount = 0;
            echo "Er is geen voldoende voorraad!";
+       }else{
+           echo "Het product is aan je winkelwagen toegevoegd!";
        }
 
         if (array_key_exists('winkelwagen', $_SESSION)) {
