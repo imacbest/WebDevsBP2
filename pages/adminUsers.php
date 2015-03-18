d6d1a263d03e6d70a2f7ab4b78606926f4716ce4
imacbest <thomas.erin999@gmail.com>
2015-03-18 09:24:31 +0100

    last bugs

--- pages/adminUsers.php
+++ pages/adminUsers.php
@@ -21,48 +21,51 @@ if(!empty($_POST['username'])) {
 
 <div id="gebruiksers">
 <?php
+if(checkIfUserIsAdmin()){
 
+    $tsql = "SELECT * FROM GEBRUIKER ";
+    $result = sqlsrv_query($db->getConn(), $tsql) or die(print_r(sqlsrv_errors()));
 
-$tsql = "SELECT * FROM GEBRUIKER ";
-$result = sqlsrv_query( $db->getConn(), $tsql) or die( print_r( sqlsrv_errors() )  );
-
-while( $row = sqlsrv_fetch_array( $result, SQLSRV_FETCH_ASSOC)){
+    while ($row = sqlsrv_fetch_array($result, SQLSRV_FETCH_ASSOC)){
     echo "<div class=\"gebruiker\">";
     $rechten = $row['RECHTEN'];
     echo "<br />";
-    if($rechten == 2) {
+    if ($rechten == 2) {
         echo "<b>Admin</b>";
     }
-    echo "  <a href='?product&amp;pid=".$row['userID']."' title='".$row['GEBRUIKERSNAAM']."'></a>
-                        <br />Naam: ".$row['VOORNAAM']." ".$row['TUSSENVOEGSEL']." ".$row['ACHTERNAAM'].
-                        "<br />Gebruiksernaam: ".$row['GEBRUIKERSNAAM']."<br />Email: ".$row['EMAIL']."<br /><br />"?>
-        <form name="deleteAccount" method="post">
+    echo "  <a href='?product&amp;pid=" . $row['userID'] . "' title='" . $row['GEBRUIKERSNAAM'] . "'></a>
+                        <br />Naam: " . $row['VOORNAAM'] . " " . $row['TUSSENVOEGSEL'] . " " . $row['ACHTERNAAM'] .
+    "<br />Gebruiksernaam: " . $row['GEBRUIKERSNAAM'] . "<br />Email: " . $row['EMAIL'] . "<br /><br />"?>
+    <form name="deleteAccount" method="post">
         <input type="submit" name="Verwijder account" value="Verwijder account"/>
         <input type="text" name="username" placeholder="username"
-               value="<?=$row['GEBRUIKERSNAAM'];?>" readonly hidden="">
-        </form>
-    <?php
-            if($rechten == 1) { ?>
-            <form name="makeAdmin" method="post">
-                <input type="submit" name="Maak administrator" value="Maak administrator"/>
-                <input type="text" name="admin" placeholder="username"
-                       value="<?=$row['GEBRUIKERSNAAM'];?>" readonly hidden="">
-                <?php
-                }
-
-
-                if($rechten == 2) { ?>
-                <form name="makeUser" method="post">
-                    <input type="submit" name="Maak gebruiker" value="Maak gebruiker"/>
-                    <input type="text" name="user" placeholder="username"
-                           value="<?=$row['GEBRUIKERSNAAM'];?>" readonly hidden="">
-                    <?php
-                    } ?>
-
+               value="<?= $row['GEBRUIKERSNAAM']; ?>" readonly hidden="">
     </form>
     <?php
-    echo "</div>";
-}
-sqlsrv_free_stmt($result);
+    if ($rechten == 1) { ?>
+    <form name="makeAdmin" method="post">
+        <input type="submit" name="Maak administrator" value="Maak administrator"/>
+        <input type="text" name="admin" placeholder="username"
+               value="<?= $row['GEBRUIKERSNAAM']; ?>" readonly hidden="">
+        <?php
+        }
+
+
+        if ($rechten == 2) { ?>
+        <form name="makeUser" method="post">
+            <input type="submit" name="Maak gebruiker" value="Maak gebruiker"/>
+            <input type="text" name="user" placeholder="username"
+                   value="<?= $row['GEBRUIKERSNAAM']; ?>" readonly hidden="">
+            <?php
+            } ?>
+
+        </form>
+        <?php
+        echo "</div>";
+        }
+        sqlsrv_free_stmt($result);
+        }else{
+            echo "Je bent geen admin, ga terug!";
+        }
 ?>
 </div>
