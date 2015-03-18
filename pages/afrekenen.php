f52ad6f7a70bc5d27aefebd65d7a88374b0c10f5
imacbest <thomas.erin999@gmail.com>
2015-03-18 09:15:51 +0100

    afreken page fix

--- pages/afrekenen.php
+++ pages/afrekenen.php
@@ -7,7 +7,7 @@ require("functies/productFuncties.php");
 if (array_key_exists('winkelwagen', $_SESSION)) {
     $items = count($_SESSION['winkelwagen']);
     if ($items > 0) {
-        echo "Producten zijn van de voorraad afgehaald!";
+        echo "Producten zijn van de voorraad afgehaald!<br />";
         $totalPrice = 0;
         for ($i = 0; $i < $items; $i++) {
             if ($_SESSION['winkelwagen'][$i]['aantal'] != 0) {
@@ -16,7 +16,7 @@ if (array_key_exists('winkelwagen', $_SESSION)) {
                 removeProductVoorraad($_SESSION['winkelwagen'][$i]['productID'], $_SESSION['winkelwagen'][$i]['aantal']);
             }
         }
-        echo "Producten zijn van de voorraad afgehaald!<br />";
+
     }
 }
 ?>
